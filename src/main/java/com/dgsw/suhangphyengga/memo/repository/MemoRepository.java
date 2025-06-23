2a85475bbdac89cdec384958182f3e352348b239
kwondongwoo0424 <144919720+kwondongwoo0424@users.noreply.github.com>
2025-06-23 15:16:22 +0900

    feat :: find memos

--- src/main/java/com/dgsw/suhangphyengga/memo/repository/MemoRepository.java
+++ src/main/java/com/dgsw/suhangphyengga/memo/repository/MemoRepository.java
@@ -1,7 +1,9 @@
 package com.dgsw.suhangphyengga.memo.repository;
 
+import com.dgsw.suhangphyengga.memo.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemoRepository {
+public interface MemoRepository extends JpaRepository<Memo,Long> {
 }
--- src/main/java/com/dgsw/suhangphyengga/memo/service/MemoServiceImpl.java
+++ src/main/java/com/dgsw/suhangphyengga/memo/service/MemoServiceImpl.java
@@ -2,21 +2,27 @@ package com.dgsw.suhangphyengga.memo.service;
 
 import com.dgsw.suhangphyengga.memo.dto.request.MemoRequest;
 import com.dgsw.suhangphyengga.memo.dto.response.MemoResponse;
+import com.dgsw.suhangphyengga.memo.entity.Memo;
+import com.dgsw.suhangphyengga.memo.repository.MemoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MemoServiceImpl implements MemoService {
+    private final MemoRepository memoRepository;
 
     @Override
     public List<MemoResponse> getAllMemo() {
-        return List.of();
+        return memoRepository.findAll().stream().map(MemoResponse::of).toList();
     }
 
     @Override
     public MemoResponse getMemo(Long id) {
-        return null;
+        Memo memo = getMemoById(id);
+        return MemoResponse.of(memo);
     }
 
     @Override
@@ -32,5 +38,10 @@ public class MemoServiceImpl implements MemoService {
     @Override
     public MemoResponse updateMemo(Long id, MemoRequest memoRequest) {
         return null;
+    }
+
+    private Memo getMemoById(Long id){
+        return memoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not found memo"));
     }
 }
