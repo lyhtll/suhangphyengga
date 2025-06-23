6d371c2e277aabb19cc6e837b4614cb9b454b9e8
lyhtll <a62346986@gmail.com>
2025-06-23 14:41:07 +0900

    init

---
+++ src/main/java/com/dgsw/suhangphyengga/SuhangphyenggaApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.suhangphyengga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhangphyenggaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuhangphyenggaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/suhangphyengga/SuhangphyenggaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.suhangphyengga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhangphyenggaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
