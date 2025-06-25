702cffa9c1af5863e00ed3aafc725f2755f4c9ae
lyhtll <a62346986@gmail.com>
2025-06-25 15:15:21 +0900

    add EnableJpaAuditing

--- src/main/java/com/dgsw/suhangphyengga/SuhangphyenggaApplication.java
+++ src/main/java/com/dgsw/suhangphyengga/SuhangphyenggaApplication.java
@@ -2,8 +2,10 @@ package com.dgsw.suhangphyengga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SuhangphyenggaApplication {
 
     public static void main(String[] args) {
